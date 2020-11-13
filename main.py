1c7e6497035caaa7d49864364f659214cb91e809
Mehdi <46991431+Mehdi-Leqsiouer@users.noreply.github.com>
2020-11-13 17:43:22 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 13 16:30:20 2020
+
+@author: mleqsiouer
+"""
+# --- Step 1: To organize the tournament ---
+
+
+def main():
+    pass
+
+if __name__ == "__main__":    
+    main()
